84d2aa421c5c8d7dd5ea4c95b5854b5efc6f97d2
Imants Horsts <imants.horsts@inbox.lv>
2016-02-08 19:24:58 +0200

    1) set Query methods as public or protected 2) extend test case class to allow testing of protected/private methods by using Reflection class 3) tried to make composer autoload-dev work without success

--- tests/QueryTest.php
+++ tests/QueryTest.php
@@ -1,35 +1,107 @@
 <?php
+namespace atk4\dsql\tests;
 
-use atk4\dsql\Query;
-
-class QueryTest extends PHPUnit_Framework_TestCase
+/**
+ * Abstract class which adds additional functionality for PHPUnit TestCase
+ * All project test classes should extend this class
+ *
+ * @todo Move this class to separate file
+ */
+abstract class AbstractTestCase extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * Name of class which we're testing with all these tests
+     */
+    public $class;
+    private $namespace = 'atk4\\dsql';
+    
+    /**
+     * Create new object of $this->class class and return it
+     * You can pass arguments to this method too
+     *
+     * @return Object
+     */
+    public function obj()
+    {
+        $class = $this->namespace . '\\' . $this->class;
+        $args = func_get_args();
+        return (new \ReflectionClass($class))->newInstanceArgs($args);
+    }
+    
+    /**
+     * Call protected/private method of a class
+     *
+     * Examples:
+     *  $this->invokeMethod('_escape', ['first_name'])
+     *  $this->invokeMethod($this->obj(['escapeChar'=>'*']), '_escape', ['first_name'])
+     *
+     * @param object $object     Optional instantiated object that we will run method on
+     * @param string $methodName Method name to call
+     * @param array  $parameters Array of parameters to pass into method
+     *
+     * @return mixed Method return.
+     */
+    public function invokeMethod($object, $methodName, array $parameters = array())
+    {
+        // $object parameter is optional
+        if (!is_object($object)) {
+            $parameters = $methodName;
+            $methodName = $object;
+            $object = $this->obj();
+        }
+        
+        // create reflection
+        $reflection = new \ReflectionClass(get_class($object));
+        $method = $reflection->getMethod($methodName);
+        $method->setAccessible(true);
 
-    function q($args = []){
-        return new Query($args);
+        return $method->invokeArgs($object, $parameters);
+    }
+}
+
+
+
+
+
+/**
+ * @coversDefaultClass \atk4\dsql\Query
+ */
+class QueryTest extends AbstractTestCase
+{
+    /**
+     * Name of class which we're testing with all these tests
+     */
+    public $class = 'Query';
+    
+    /**
+     * @covers ::_escape
+     */
+    public function testEscaping()
+    {
+        // escaping expressions
+        $this->assertEquals('`first_name`',     $this->invokeMethod('_escape', ['first_name']));
+        $this->assertEquals('*first_name*',     $this->invokeMethod($this->obj(['escapeChar'=>'*']), '_escape', ['first_name']));
+        
+        // should not escape expressions
+        $this->assertEquals('*',                $this->invokeMethod('_escape', ['*']));
+        $this->assertEquals('(2+2) age',        $this->invokeMethod('_escape', ['(2+2) age']));
+        $this->assertEquals('first_name.table', $this->invokeMethod('_escape', ['first_name.table']));
+        $this->assertEquals('first#name',       $this->invokeMethod($this->obj(['escapeChar'=>'#']), '_escape', ['first#name']));
+        $this->assertEquals(true,               is_object($this->invokeMethod('_escape', [$this->obj()])));
+        
+        // escaping array - escapes each of its elements
+        $this->assertEquals(
+                json_encode(['`first_name`','*','`last_name`']),
+                json_encode($this->invokeMethod('_escape', [ ['first_name', '*', 'last_name'] ]))
+            );
     }
 
-    function testTable()
+    /**
+     * @covers ::field
+     */
+    /*
+    public function testFieldBasic()
     {
-        $q = new Query();
-        $res = $q->table('employee');
-        $this->assertEquals(true, $res);
-    }
-
-    function testEscaping()
-    {
-        // escaping exclusions
-        $this->assertEquals('`first_name`', $this->q()->_escape('first_name'));
-        $this->assertEquals('*first_name*', $this->q(['escapeChar'=>'*'])->_escape('first_name'));
-        $this->assertEquals('first_name.table', $this->q()->_escape('first_name.table'));
-        $this->assertEquals('(2+2) age', $this->q()->_escape('(2+2) age'));
-        $this->assertEquals('*', $this->q()->_escape('*'));
-
-    }
-
-    function testFieldBasic()
-    {
-        // excludes expressions
         $this->assertEquals('`first_name`', $this->q()->field('first_name')->_render_field());
         $this->assertEquals('`first_name`,`last_name`', $this->q()->field('first_name,last_name')->_render_field());
         $this->assertEquals('`emplayee`.`first_name`', $this->q()->field('first_name','emplayee')->_render_field());
@@ -43,5 +115,15 @@ class QueryTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('first_name.employee', $this->q()->field('first_name.employee')->_render_field());
 
     }
+    */
 
+    /**
+     * @covers ::table
+     */
+    /*
+    public function testTable()
+    {
+        $this->assertEquals(true, $this->invokeMethod('table', ['employee']));
+    }
+    */
 }
