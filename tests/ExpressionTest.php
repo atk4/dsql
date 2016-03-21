a5e236bbe3844f23c9ad2e9bc4e074fa21214bf9
Imants Horsts <imants.horsts@inbox.lv>
2016-03-21 14:48:30 +0200

    blah, let's try one more time to fix this test

--- tests/ExpressionTest.php
+++ tests/ExpressionTest.php
@@ -283,7 +283,7 @@ class ExpressionTest extends \PHPUnit_Framework_TestCase
         );
         $this->assertEquals(
             true,
-            PHPUnitUtil::callProtectedMethod($this->e(), '_escape', [new stdClass()]) instanceof stdClass
+            PHPUnitUtil::callProtectedMethod($this->e(), '_escape', [new \stdClass()]) instanceof \stdClass
         );
 
         // escaping array - escapes each of its elements
