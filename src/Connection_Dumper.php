7c5ecf46d195d91bd19e15dd583116b8ce741fc2
Romans Malinovskis <romans@agiletoolkit.org>
2016-07-21 14:18:12 +0100

    Output message to STDERR, more appropriate.

--- src/Connection_Dumper.php
+++ src/Connection_Dumper.php
@@ -22,7 +22,11 @@ class Connection_Dumper extends Connection_Proxy
             $c = $this->callback;
             $c($expr, $took);
         } else {
-            printf("[%02.6f] %s\n", $took, $expr->getDebugQuery());
+            $stderr = fopen('php://stderr', 'w'); 
+            $Message =  sprintf("[%02.6f] %s\n", $took, $expr->getDebugQuery());
+            fwrite($stderr,$Message); 
+            fclose($stderr); 
+            
         }
 
         return $ret;
