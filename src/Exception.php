d3b123bb3582f6201189866c827b75cdeaeb8151
DarkSide <imants.horsts@inbox.lv>
2016-08-05 00:56:45 +0300

    use \atk4\core\Exception

--- src/Exception.php
+++ src/Exception.php
@@ -10,40 +10,6 @@ namespace atk4\dsql;
  * @license MIT
  * @copyright Agile Toolkit (c) http://agiletoolkit.org/
  */
-class Exception extends \Exception
+class Exception extends \atk4\core\Exception
 {
-    /**
-     * Most exceptions would be a cause by some other exception, DSQL
-     * will encapsulate them and allow you to access them anyway.
-     */
-    private $params = [];
-
-    /**
-     * @param string     $message
-     * @param int        $code
-     * @param \Throwable $previous
-     */
-    public function __construct(
-        $message = '',
-        $code = 0,
-        \Throwable $previous = null
-    ) {
-        if (is_array($message)) {
-            // message contain additional parameters
-            $this->params = $message;
-            $message = array_shift($this->params);
-        }
-
-        parent::__construct($message, $code, $previous);
-    }
-
-    /**
-     * Follow the getter-style of PHP Exception.
-     *
-     * @return array
-     */
-    public function getParams()
-    {
-        return $this->params;
-    }
 }
