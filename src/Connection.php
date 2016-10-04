8d8ae4753143b653bb4da9d8b1990a9a431cebab
DarkSide <imants.horsts@inbox.lv>
2016-10-04 23:44:02 +0300

    add comment

--- src/Connection.php
+++ src/Connection.php
@@ -160,6 +160,10 @@ class Connection
      * Atomic executes operations within one begin/end transaction, so if
      * the code inside callback will fail, then all of the transaction
      * will be also rolled back.
+     *
+     * @param callable $f
+     *
+     * @return mixed
      */
     public function atomic($f)
     {
