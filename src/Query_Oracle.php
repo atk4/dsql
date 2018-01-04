d89fa60194d0dc7911c5532ea513ce2f230cd48b
DarkSide <imants.horsts@inbox.lv>
2018-01-05 01:35:26 +0200

    more duplicated code

--- src/Query_Oracle.php
+++ src/Query_Oracle.php
@@ -39,36 +39,4 @@ class Query_Oracle extends Query_Oracle_Abstract
         return ' and "__dsql_rownum"<='.
             ((int) ($this->args['limit']['cnt'] + $this->args['limit']['shift']));
     }
-
-    public function field($field, $alias = null)
-    {
-        // field is passed as string, may contain commas
-        if (is_string($field) && strpos($field, ',') !== false) {
-            $field = explode(',', $field);
-        }
-
-        // recursively add array fields
-        if (is_array($field)) {
-            if ($alias !== null) {
-                throw new Exception([
-                    'Alias must not be specified when $field is an array',
-                    'alias' => $alias,
-                ]);
-            }
-
-            foreach ($field as $alias => $f) {
-                if (is_numeric($alias)) {
-                    $alias = null;
-                }
-                $this->field($f, $alias);
-            }
-
-            return $this;
-        }
-
-        // save field in args
-        $this->_set_args('field', $alias, $field);
-
-        return $this;
-    }
 }
