9afcbe22b671084457f51cc3a81ac1e47268741d
Imants Horsts <DarkSide666@users.noreply.github.com>
2018-12-28 21:11:30 +0200

    fix test

--- tests/ConnectionTest.php
+++ tests/ConnectionTest.php
@@ -59,10 +59,10 @@ class ConnectionTest extends \atk4\core\PHPUnit_AgileTestCase
 
         // with port number
         $dsn = Connection::normalizeDSN('mysql://root:pass@localhost:1234/db');
-        $this->assertEquals(['dsn'=>'mysql:host=localhost:1234;dbname=db', 'user'=>'root', 'pass'=>'pass', 'driver'=>'mysql', 'rest'=>'host=localhost:1234;dbname=db'], $dsn);
+        $this->assertEquals(['dsn'=>'mysql:host=localhost;port=1234;dbname=db', 'user'=>'root', 'pass'=>'pass', 'driver'=>'mysql', 'rest'=>'host=localhost;port=1234;dbname=db'], $dsn);
 
         $dsn = Connection::normalizeDSN('mysql:host=localhost:1234;dbname=db');
-        $this->assertEquals(['dsn'=>'mysql:host=localhost:1234;dbname=db', 'user'=>null, 'pass'=>null, 'driver'=>'mysql', 'rest'=>'host=localhost:1234;dbname=db'], $dsn);
+        $this->assertEquals(['dsn'=>'mysql:host=localhost;port=1234;dbname=db', 'user'=>null, 'pass'=>null, 'driver'=>'mysql', 'rest'=>'host=localhost;port=1234;dbname=db'], $dsn);
     }
 
     public function testDumper()
