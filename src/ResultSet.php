98fbc6464da523a7575b7fc17183fde735d2b3c7
Romans Malinovskis <me@nearly.guru>
2019-01-09 15:03:10 +0000

    Apply fixes from StyleCI

--- src/ResultSet.php
+++ src/ResultSet.php
@@ -7,13 +7,15 @@ namespace atk4\dsql;
 /**
  * This interface describes a class, which can represents a result set. Some operations can be performed with this, such as
  * counting or adding conditions. This is implemented by Expression but more importantly ATK4 Data adds more implementations
- * for the ResultSet which implement NoSQL actions (such as on arrays etc)
+ * for the ResultSet which implement NoSQL actions (such as on arrays etc).
  */
 interface ResultSet
 {
     //public function where($field, $value);
     //public function count();
     public function get();
+
     public function getRow();
+
     public function getOne();
 }
