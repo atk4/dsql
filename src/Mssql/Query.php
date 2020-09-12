8f71310bb9c474c6e7ecdf8c16e43b6eb2629631
Michael Voříšek <mvorisek@mvorisek.cz>
2020-09-12 11:46:48 +0200

    Add MSSQL support (#245)
    * fix limit clause
    
    * fix explicit insert ID
    
    * getDebugQuery() must not replace numbers
    
    * impl. Query::groupConcat() for MSSQL
    
    * Add MSSQL CI testing
    
    * fix typo

--- src/Mssql/Query.php
+++ src/Mssql/Query.php
@@ -13,13 +13,32 @@ class Query extends BaseQuery
 {
     use ExpressionTrait;
 
-    /**
-     * Field, table and alias name escaping symbol.
-     *
-     * @var string
-     */
     protected $escape_char = ']';
 
-    /** @var string Expression classname */
     protected $expression_class = Expression::class;
+
+    protected $template_insert = 'begin try'
+        . "\n" . 'insert[option] into [table_noalias] ([set_fields]) values ([set_values])'
+        . "\n" . 'end try begin catch if ERROR_NUMBER() = 544 begin'
+        . "\n" . 'set IDENTITY_INSERT [table_noalias] on'
+        . "\n" . 'insert[option] into [table_noalias] ([set_fields]) values ([set_values])'
+        . "\n" . 'set IDENTITY_INSERT [table_noalias] off'
+        . "\n" . 'end end catch';
+
+    public function _render_limit()
+    {
+        if (isset($this->args['limit'])) {
+            $cnt = (int) $this->args['limit']['cnt'];
+            $shift = (int) $this->args['limit']['shift'];
+
+            return (!isset($this->args['order']) ? ' order by (select null)' : '')
+                . ' offset ' . $shift . ' rows'
+                . ' fetch next ' . $cnt . ' rows only';
+        }
+    }
+
+    public function groupConcat($field, $delimeter = ',')
+    {
+        return $this->expr('string_agg({}, \'' . $delimeter . '\')', [$field]);
+    }
 }
