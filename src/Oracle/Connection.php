a65db28b7f5c9185871b70177b44c0d59dbb66d2
Michael Voříšek <mvorisek@mvorisek.cz>
2020-09-15 14:09:55 +0200

    Reuse previous PDO connection for CI (#253)

--- src/Oracle/Connection.php
+++ src/Oracle/Connection.php
@@ -16,8 +16,6 @@ class Connection extends BaseConnection
     /** @var string Query classname */
     protected $query_class = Query::class;
 
-    private static $_killerHack = [];
-
     /**
      * Add some configuration for current connection session.
      *
@@ -33,22 +31,58 @@ class Connection extends BaseConnection
             'date_format' => 'YYYY-MM-DD', // date format
             'dec_char' => '. ', // decimal separator, no thousands separator
         ])->execute();
-
-        // TODO hack to pass tests, currently, something prevents previous connection instances to be released
-        // and thus high number of concurrent connections is active
-        self::$_killerHack[] = PHP_MAJOR_VERSION > 7 || PHP_MINOR_VERSION === 4 ? \WeakReference::create($this) : $this;
-        while (count(self::$_killerHack) > 50) {
-            $c = array_shift(self::$_killerHack);
-            if ($c instanceof \WeakReference) {
-                $c = $c->get();
-            }
-
-            if ($c !== null) {
-                $c->connection = null;
-            }
-        }
-        gc_collect_cycles();
     }
+
+    // {{{ fix for too many connections for CI testing
+
+    /** @var array */
+    private static $ciLastConnectDsn;
+    /** @var \PDO */
+    private static $ciLastConnectPdo;
+
+    protected static function connectDriver(array $dsn)
+    {
+        // for some reasons, the following error:
+        // PDOException: SQLSTATE[HY000]: pdo_oci_handle_factory: ORA-12516: TNS:listener could not find available handler with matching protocol stack
+        // is shown randomly when a lot of connections are created in tests,
+        // so for CI, fix this issue by reusing the previous PDO connection
+        // TODO remove once atk4/data tests can be run consistently without errors
+        if (class_exists(\PHPUnit\Framework\TestCase::class, false)) { // called from phpunit
+            $notReusableFunc = function (string $message): void {
+                echo "\n" . 'connection for CI can not be reused:' . "\n" . $message . "\n";
+                self::$ciLastConnectPdo = null;
+            };
+
+            if (self::$ciLastConnectDsn !== $dsn) {
+                $notReusableFunc('different DSN');
+            } elseif (self::$ciLastConnectPdo !== null) {
+                try {
+                    self::$ciLastConnectPdo->query('select 1 from dual')->fetch();
+                } catch (\PDOException $e) {
+                    $notReusableFunc((string) $e);
+                }
+            }
+
+            if (self::$ciLastConnectPdo !== null && self::$ciLastConnectPdo->inTransaction()) {
+                $notReusableFunc('inside transaction');
+            }
+
+            if (self::$ciLastConnectPdo !== null) {
+                $pdo = self::$ciLastConnectPdo;
+            } else {
+                $pdo = parent::connectDriver($dsn);
+            }
+
+            self::$ciLastConnectPdo = $pdo;
+            self::$ciLastConnectDsn = $dsn;
+
+            return $pdo;
+        }
+
+        return parent::connectDriver($dsn);
+    }
+
+    /// }}}
 
     /**
      * Return last inserted ID value.
